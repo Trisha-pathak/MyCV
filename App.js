e6c8fe1e63242bf2e77d96d28c90d0410ea7fd31
Trisha-pathak <68096593+Trisha-pathak@users.noreply.github.com>
2022-06-13 18:16:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CVnav from './components/CV/CVnav'
+import CVcontent from './components/CV/CVcontent';
+
+function App() {
+  return (
+    <>
+    <CVnav />
+    <CVcontent />
+    </>
+  );
+}
+
+export default App;
